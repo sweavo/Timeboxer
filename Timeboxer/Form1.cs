1e9db1714e79cdf6fb6d6cdf469d614e3cc2e65e
Steve Carter <sweavo@gmail.com>
2020-02-18 21:04:06 +0000

    fixed the input part

--- Timeboxer/Form1.cs
+++ Timeboxer/Form1.cs
@@ -14,9 +14,8 @@ namespace Timeboxer
 {
     public partial class TimeboxerForm : Form
     {
-        private DateTime alarm = DateTime.Now;
-        private PointF last_mouse = new Point(0, 0);
-
+        private DateTime alarm_time = DateTime.Now;
+        private double mouse_angle= 0.0;
         public TimeboxerForm()
         {
             InitializeComponent();
@@ -42,6 +41,8 @@ namespace Timeboxer
 
             // Get the hour and minute plus any fraction that has elapsed.
             DateTime now = DateTime.Now;
+            TimeSpan ts = alarm_time - now;
+            float sweep = 6f * (float)(ts.TotalMinutes);
 
             // We're going to draw arcs and pies in a common rectangle, which i the client rectangle minus some padding:
             Rectangle pad_rectangle = new Rectangle(
@@ -51,7 +52,7 @@ namespace Timeboxer
             // Draw the second hand.
             gr.FillPie(Brushes.Coral,
                 pad_rectangle,
-                -90.0f, 6*(now.Second % 60));
+                -90.0f, sweep);
 
             // Outline
             gr.DrawEllipse(border_pen,
@@ -89,21 +90,39 @@ namespace Timeboxer
                     gr.DrawLine(thin_tick_pen, inner_pt, outer_pt);
                 }
             }
+            TextRenderer.DrawText(gr, ts.TotalSeconds.ToString(), this.Font, ClientRectangle, Color.Black, Color.Yellow, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
 
-            // Draw the mouse position
-            gr.FillRectangle(Brushes.Green, last_mouse.X, last_mouse.Y, 5,5);
-            // Draw _through_ the mouse position
-            double mouse_angle = Math.Atan(last_mouse.Y / last_mouse.X) * 180 / Math.PI + (last_mouse.X < 0 ? 270 : 90 );
-            gr.FillPie(Brushes.AliceBlue, pad_rectangle, -90.0f, (float)mouse_angle);
-            
+        }
+
+        // Return angle from origin to point in positive degrees
+        private double get_angle_from_vector(Point p)
+        {
+            double first_try = Math.Atan2(p.X, -p.Y) * (180 / Math.PI);
+            if ( first_try < 0.0 )
+            {
+                return first_try + 360 ;
+            }
+            else
+            {
+                return first_try;
+            }
+
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button==MouseButtons.Left)
+            Point mouse_at = e.Location;
+            // Convert to transformed coords
+            mouse_at.X = mouse_at.X - ClientRectangle.Width / 2;
+            mouse_at.Y = mouse_at.Y - ClientRectangle.Height / 2;
+
+            if (e.Button == MouseButtons.Left)
             {
-                last_mouse.X = e.Location.X-ClientRectangle.Width/2;
-                last_mouse.Y = e.Location.Y - ClientRectangle.Height / 2;
+                // Get angle through the mouse position
+                mouse_angle = get_angle_from_vector(mouse_at);
+
+                double mouse_period = mouse_angle / 6.0;
+                alarm_time = DateTime.Now.AddMinutes(mouse_period);
             }
 
             Refresh();
