6590ea9bd8cfc6f7b5ad23e00fa9ee88b600f862
Steve Carter <sweavo@gmail.com>
2020-02-18 16:52:42 +0000

    draw a clock face

--- Timeboxer/Form1.cs
+++ Timeboxer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,63 @@ namespace Timeboxer
         public TimeboxerForm()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+
+            // Put origin in centre of client area
+            gr.TranslateTransform(
+                ClientSize.Width / 2,
+                ClientSize.Height / 2);
+
+            using (Pen thick_pen = new Pen(Color.Blue, 4))
+            {
+                // Outline
+                gr.DrawEllipse(thick_pen, 
+                    -ClientSize.Width/2f+4f, -ClientSize.Height/2f + 4f, 
+                    ClientSize.Width-8f, ClientSize.Height-8f);
+
+                // Get scale factors.
+                float outer_x_factor = 0.45f * ClientSize.Width;
+                float outer_y_factor = 0.45f * ClientSize.Height;
+                float inner_x_factor = 0.425f * ClientSize.Width;
+                float inner_y_factor = 0.425f * ClientSize.Height;
+                float big_x_factor = 0.4f * ClientSize.Width;
+                float big_y_factor = 0.4f * ClientSize.Height;
+
+                // Draw the tick marks.
+                thick_pen.StartCap = LineCap.Triangle;
+                for (int minute = 1; minute <= 60; minute++)
+                {
+                    double angle = Math.PI * minute / 30.0;
+                    float cos_angle = (float)Math.Cos(angle);
+                    float sin_angle = (float)Math.Sin(angle);
+                    PointF outer_pt = new PointF(
+                        outer_x_factor * cos_angle,
+                        outer_y_factor * sin_angle);
+                    if (minute % 5 == 0)
+                    {
+                        PointF inner_pt = new PointF(
+                            big_x_factor * cos_angle,
+                            big_y_factor * sin_angle);
+                        gr.DrawLine(thick_pen, inner_pt, outer_pt);
+                    }
+                    else
+                    {
+                        PointF inner_pt = new PointF(
+                            inner_x_factor * cos_angle,
+                            inner_y_factor * sin_angle);
+                        gr.DrawLine(Pens.Blue, inner_pt, outer_pt);
+                    }
+                }
+            }
         }
     }
 }
