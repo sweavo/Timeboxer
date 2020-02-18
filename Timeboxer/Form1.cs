1f6cf265fd54c0507b763be0d721ac95736b7b9c
Steve Carter <sweavo@gmail.com>
2020-02-18 17:08:17 +0000

    refactored all the pens

--- Timeboxer/Form1.cs
+++ Timeboxer/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,57 +26,63 @@ namespace Timeboxer
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
+            Pen border_pen = new Pen(Color.Black, 4);
+
+            Pen thick_tick_pen = new Pen(Color.Black, 4);
+            thick_tick_pen.StartCap = LineCap.Triangle;
+
+            Pen thin_tick_pen = Pens.Black;
+
+            Pen hand_pen = new Pen(Color.Red, 2);
+
             Graphics gr = e.Graphics;
+            gr.SmoothingMode = SmoothingMode.AntiAlias;
+            gr.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
 
             // Put origin in centre of client area
             gr.TranslateTransform(
                 ClientSize.Width / 2,
                 ClientSize.Height / 2);
 
-            using (Pen thick_pen = new Pen(Color.Black, 4))
+            // Outline
+            gr.DrawEllipse(border_pen,
+                -ClientSize.Width / 2f + 4f, -ClientSize.Height / 2f + 4f,
+                ClientSize.Width - 8f, ClientSize.Height - 8f);
+
+            // Get scale factors.
+            float outer_x_factor = 0.45f * ClientSize.Width;
+            float outer_y_factor = 0.45f * ClientSize.Height;
+            float inner_x_factor = 0.425f * ClientSize.Width;
+            float inner_y_factor = 0.425f * ClientSize.Height;
+            float big_x_factor = 0.4f * ClientSize.Width;
+            float big_y_factor = 0.4f * ClientSize.Height;
+
+            // Draw the tick marks.
+            for (int minute = 1; minute <= 60; minute++)
             {
-                // Outline
-                gr.DrawEllipse(thick_pen, 
-                    -ClientSize.Width/2f+4f, -ClientSize.Height/2f + 4f, 
-                    ClientSize.Width-8f, ClientSize.Height-8f);
-
-                // Get scale factors.
-                float outer_x_factor = 0.45f * ClientSize.Width;
-                float outer_y_factor = 0.45f * ClientSize.Height;
-                float inner_x_factor = 0.425f * ClientSize.Width;
-                float inner_y_factor = 0.425f * ClientSize.Height;
-                float big_x_factor = 0.4f * ClientSize.Width;
-                float big_y_factor = 0.4f * ClientSize.Height;
-
-                // Draw the tick marks.
-                thick_pen.StartCap = LineCap.Triangle;
-                for (int minute = 1; minute <= 60; minute++)
+                double angle = Math.PI * minute / 30.0;
+                float cos_angle = (float)Math.Cos(angle);
+                float sin_angle = (float)Math.Sin(angle);
+                PointF outer_pt = new PointF(
+                    outer_x_factor * cos_angle,
+                    outer_y_factor * sin_angle);
+                if (minute % 5 == 0)
                 {
-                    double angle = Math.PI * minute / 30.0;
-                    float cos_angle = (float)Math.Cos(angle);
-                    float sin_angle = (float)Math.Sin(angle);
-                    PointF outer_pt = new PointF(
-                        outer_x_factor * cos_angle,
-                        outer_y_factor * sin_angle);
-                    if (minute % 5 == 0)
-                    {
-                        PointF inner_pt = new PointF(
-                            big_x_factor * cos_angle,
-                            big_y_factor * sin_angle);
-                        gr.DrawLine(thick_pen, inner_pt, outer_pt);
-                    }
-                    else
-                    {
-                        PointF inner_pt = new PointF(
-                            inner_x_factor * cos_angle,
-                            inner_y_factor * sin_angle);
-                        gr.DrawLine(Pens.Blue, inner_pt, outer_pt);
-                    }
+                    PointF inner_pt = new PointF(
+                        big_x_factor * cos_angle,
+                        big_y_factor * sin_angle);
+                    gr.DrawLine(thick_tick_pen, inner_pt, outer_pt);
+                }
+                else
+                {
+                    PointF inner_pt = new PointF(
+                        inner_x_factor * cos_angle,
+                        inner_y_factor * sin_angle);
+                    gr.DrawLine(thin_tick_pen, inner_pt, outer_pt);
                 }
             }
 
-            using (Pen thick_pen = new Pen(Color.Red, 4))
-            {
+
                 // Get the hour and minute plus any fraction that has elapsed.
                 DateTime now = DateTime.Now;
 
@@ -88,8 +95,8 @@ namespace Timeboxer
                 PointF second_pt = new PointF(
                     (float)(second_x_factor * Math.Cos(second_angle)),
                     (float)(second_y_factor * Math.Sin(second_angle)));
-                gr.DrawLine(Pens.Red, second_pt, center);
-            }
+                gr.DrawLine(hand_pen, second_pt, center);
+            
         }
     }
 }
