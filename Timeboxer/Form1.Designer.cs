0eaee5f122f313dfed62976e48c53711c7709f6e
Steve Carter <sweavo@gmail.com>
2020-02-18 16:46:57 +0000

    design the form

--- Timeboxer/Form1.Designer.cs
+++ Timeboxer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Timeboxer
 {
-    partial class Form1
+    partial class TimeboxerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TimeboxerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "TimeboxerForm";
+            this.Text = "Timeboxer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Timeboxer/Form1.cs
+++ Timeboxer/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Timeboxer
 {
-    public partial class Form1 : Form
+    public partial class TimeboxerForm : Form
     {
-        public Form1()
+        public TimeboxerForm()
         {
             InitializeComponent();
         }
