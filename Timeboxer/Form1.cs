ae8383ff9893cc7bb9a37863eb9f2acf738fb5ea
Steve Carter <sweavo@gmail.com>
2020-02-19 09:26:31 +0000

    extract the drawing of ticks

--- Timeboxer/Form1.cs
+++ Timeboxer/Form1.cs
@@ -15,12 +15,12 @@ namespace Timeboxer
 
         private Brush face_brush = Brushes.White;
         private Brush pie_brush = Brushes.Coral;
-        private Pen border_pen= new Pen(Color.Black, 4);
+        private Pen border_pen = new Pen(Color.Black, 4);
         private Pen thick_tick_pen; // see ctor
         private Pen thin_tick_pen = Pens.Black;
 
         // radii of ticks, as a proportion of the available radius
-        private float big_tick_r_from=0.8f;
+        private float big_tick_r_from = 0.8f;
         private float small_tick_r_from = 0.85f;
         private float tick_r_to = 0.9f;
 
@@ -47,25 +47,39 @@ namespace Timeboxer
                 return Math.Max((float)ts.TotalSeconds, 0f);
             }
         }
-        
+
         // Sweep: number of degrees (360 deg =1 hour) remaining
         private float Sweep
         {
             get
             {
-                return (float)(RemainingSeconds/10.0f);
+                return (float)(RemainingSeconds / 10.0f);
 
             }
         }
 
-        private void draw_tick(Pen p, double angle, float inner_r, float outer_r)
+        private void draw_tick(Graphics gr, Pen p, double angle, float inner_r, float outer_r)
         {
+            float outer_x_factor = outer_r * ClientSize.Width / 2;
+            float outer_y_factor = outer_r * ClientSize.Height / 2;
+            float inner_x_factor = inner_r * ClientSize.Width / 2;
+            float inner_y_factor = inner_r * ClientSize.Height / 2;
+            float cos_angle = (float)Math.Cos(angle);
+            float sin_angle = (float)Math.Sin(angle);
+            PointF outer_pt = new PointF(
+                outer_x_factor * cos_angle,
+                outer_y_factor * sin_angle);
+            PointF inner_pt = new PointF(
+                inner_x_factor * cos_angle,
+                inner_y_factor * sin_angle);
+            gr.DrawLine(p, inner_pt, outer_pt);
+
         }
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
 
             Graphics gr = e.Graphics;
-            gr.Clear(this.TransparencyKey); 
+            gr.Clear(this.TransparencyKey);
 
             gr.SmoothingMode = SmoothingMode.AntiAlias;
             gr.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
@@ -84,20 +98,12 @@ namespace Timeboxer
 
             // Clock Face
             gr.FillEllipse(face_brush, pad_rectangle);
-            
+
             // Draw the sweep
             gr.FillPie(pie_brush, pad_rectangle, FACE_ANGLE_TOP, this.Sweep);
 
             // Bezel
             gr.DrawEllipse(border_pen, pad_rectangle);
-
-            // Get scale factors.
-            float outer_x_factor = tick_r_to * ClientSize.Width/2;
-            float outer_y_factor = tick_r_to * ClientSize.Height/2;
-            float inner_x_factor = small_tick_r_from * ClientSize.Width/2;
-            float inner_y_factor = small_tick_r_from * ClientSize.Height/2;
-            float big_x_factor = big_tick_r_from * ClientSize.Width/2;
-            float big_y_factor = big_tick_r_from * ClientSize.Height/2;
 
             // Draw the tick marks.
             for (int minute = 1; minute <= 60; minute++)
@@ -105,27 +111,11 @@ namespace Timeboxer
                 double angle = Math.PI * minute / 30.0;
                 if (minute % 5 == 0)
                 {
-                    float cos_angle = (float)Math.Cos(angle);
-                    float sin_angle = (float)Math.Sin(angle);
-                    PointF outer_pt = new PointF(
-                        outer_x_factor * cos_angle,
-                        outer_y_factor * sin_angle);
-                    PointF inner_pt = new PointF(
-                        big_x_factor * cos_angle,
-                        big_y_factor * sin_angle);
-                    gr.DrawLine(thick_tick_pen, inner_pt, outer_pt);
+                    draw_tick(gr, thick_tick_pen, angle, big_tick_r_from, tick_r_to);
                 }
                 else
                 {
-                    float cos_angle = (float)Math.Cos(angle);
-                    float sin_angle = (float)Math.Sin(angle);
-                    PointF outer_pt = new PointF(
-                        outer_x_factor * cos_angle,
-                        outer_y_factor * sin_angle);
-                    PointF inner_pt = new PointF(
-                        inner_x_factor * cos_angle,
-                        inner_y_factor * sin_angle);
-                    gr.DrawLine(thin_tick_pen, inner_pt, outer_pt);
+                    draw_tick(gr, thin_tick_pen, angle, small_tick_r_from, tick_r_to);
                 }
             }
             //TextRenderer.DrawText(gr, ((int)ts.TotalSeconds).ToString(), this.Font, ClientRectangle, Color.Black, Color.WhiteSmoke, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter );
@@ -137,9 +127,9 @@ namespace Timeboxer
         private double get_angle_from_vector(Point p)
         {
             double first_try = Math.Atan2(p.X, -p.Y) * (180 / Math.PI);
-            if ( first_try < 0.0 )
+            if (first_try < 0.0)
             {
-                return first_try + 360 ;
+                return first_try + 360;
             }
             else
             {
