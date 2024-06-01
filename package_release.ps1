5c9214f3ac28a228e0897f01ef68eef97c2d332b
Steve <sweavo@gmail.com>
2024-06-01 08:42:18 +0100

    readme and devlog; fix up the project and release process

--- package_release.ps1
+++ package_release.ps1
@@ -1,3 +1,16 @@
+# Prepare a release package for upload to github
+#
+# DOES NOT BUILD OR TEST.
+#
+# Process is: 
+#
+# * In visual studio, use the project properties to set the version number
+# * rebuild project in Release Build
+# * Make sure the README talks about the latest features
+# * Test that you can drag the .exe someplace else and start it from there
+# * run this script. It reads the version resource from the exe and uses it to update the readme and to name the zipfile. It removes and replaces the Staging/ folder.
+# * Github things
+#
 
 $version = (Get-Item .\Timeboxer\bin\Release\Timeboxer.exe).VersionInfo.FileVersionRaw
 
